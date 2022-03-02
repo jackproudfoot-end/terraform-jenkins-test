89d6d7c123919b8401a8f2d1731eaa4f7e466f82
Jack END <jackend@Jacks-MacBook-Pro-3.local>
2022-03-02 11:33:50 -0500

    Added archive file step

--- helloworld/main.tf
+++ helloworld/main.tf
@@ -1,3 +1,9 @@
+data "archive_file" "hello_world_text" {
+	type = "zip"
+	source_file = "${path.module}/helloworld.txt"
+	output_path = "${path.module}/files/helloworld.zip"
+}
+
 output "hello_world" {
 	value = "Hello, World!"
 }
