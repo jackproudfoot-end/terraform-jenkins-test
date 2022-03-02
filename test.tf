acc2f5ea73d4e84935543ad9179a28591a390790
Jack Proudfoot <jackson@end-labs.io>
2022-03-02 16:18:20 -0500

    Removed terraform

--- test.tf
+++
@@ -1,9 +0,0 @@
-data "archive_file" "hello_world_text" {
-	type = "zip"
-	source_file = "${path.module}/helloworld.txt"
-	output_path = "${path.module}/files/helloworld.zip"
-}
-
-output "hello_world" {
-	value = "Hello, World!"
-}
